917d5dc3658388f7da2303da6ef3d5a5f900dc45
danilapower <danil.duzhnikov@mail.ru>
2026-05-20 12:09:47 +0000

    added user + admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    login = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.username
